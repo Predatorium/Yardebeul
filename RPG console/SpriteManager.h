d510f0e825682ff49a32035f68451108631a2227
Predatorium <laurent.preda@gmail.com>
2021-02-15 18:29:38 +0100

    Salut

--- RPG console/SpriteManager.h
+++ RPG console/SpriteManager.h
@@ -4,7 +4,7 @@
 
 class Sprites
 {
-public:
+public :
 	Sprites() = default;
 	Sprites(string Name, string Path);
 	~Sprites() = default;
@@ -12,10 +12,10 @@ public:
 	string getName() const { return m_name; };
 	Texture& getTexture() { return m_texture; };
 
-	bool operator == (const Sprites& s) const { return m_name == s.m_name; }
-	bool operator != (const Sprites& s) const { return !operator==(s); }
+	bool operator ==(const Sprites& s) const { return m_name == s.m_name; }
+	bool operator !=(const Sprites& s) const { return m_name != s.m_name; }
 
-private:
+private :
 	string m_name;
 	Texture m_texture;
 };
